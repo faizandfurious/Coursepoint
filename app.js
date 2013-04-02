e36defef193d4b0262a2ab71a6a892326ae7133d
Faiz <faizamodo@gmail.com>
2013-04-02 14:12:03 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express"); // imports express
+var app = express();        // create a new instance of express
+
+// This is for serving files in the static directory
+app.get("/static/:staticFilename", function (request, response) {
+    response.sendfile("static/" + request.params.staticFilename);
+});
+
+// This is for serving files in the static directory
+app.get("/static/styles/:staticFilename", function (request, response) {
+    response.sendfile("static/styles/" + request.params.staticFilename);
+});
+
+// This is for serving files in the static directory
+app.get("/static/views/:staticFilename", function (request, response) {
+    response.sendfile("static/views/" + request.params.staticFilename);
+});
+
+// This is for serving files in the static directory
+app.get("/static/js/:staticFilename", function (request, response) {
+    response.sendfile("static/js/" + request.params.staticFilename);
+});
+
+function initServer() {
+}
+
+// Finally, initialize the server, then activate the server at port 8889
+initServer();
+app.listen(8889);
