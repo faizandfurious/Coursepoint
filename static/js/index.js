2b4e23bd96f8b99cba460888b915241ad7e5a11f
Faiz <faizamodo@gmail.com>
2013-04-15 22:42:08 -0400

    Added quiz page

--- static/js/index.js
+++ static/js/index.js
@@ -5,12 +5,29 @@ $(document).ready(function(){
 
 $("#menu_button").click(function(){
     console.log("menu button clicked");
-})
+});
 
 
 $("#add_class_button").click(function(){
     console.log("add class button clicked");
-})
+});
+
+$("#0").click(function(){
+    console.log("test");
+    //Put in request to get class information
+    getClass(0);
+    window.location = "class.html";//url
+});
+
+function getClass(id){
+    $.ajax({
+        type: "get",
+        url:"/get_class" + id,
+        success: function(data){
+            console.log(data);
+        }
+    })
+}
 
 
 function getData(){
