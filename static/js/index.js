bc97f3da2730b1ecf8179664a72f91369e8a7fc4
Faiz <faizamodo@gmail.com>
2013-04-16 13:15:37 -0400

    Fixed the background image problem between login and other screens

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,8 @@
 var login_page = $("#login_content");
 var class_list_page = $("#class_list_content");
 var quiz_page = $("#quiz_content");
+var menu_button = $("#menu_button");
+var add_class_button = $("#add_class_button");
 
 
 $(document).ready(function(){
@@ -9,17 +11,61 @@ $(document).ready(function(){
     getData();
 });
 
+function hideTopbarButtons(){
+    menu_button.hide();
+    add_class_button.hide();
+}
+
+function showTopbarButtons(){
+    menu_button.show();
+    add_class_button.show();
+}
+
 function showLogin(){
     class_list_page.hide();
     quiz_page.hide();
     login_page.show();
+    hideTopbarButtons();
+    addGatesImageToBody();
 }
 
+function addGatesImageToBody(){
+    $("body").css({
+        'background-image': 'url("../styles/images/ghc.jpg")',
+        'background-repeat':'no-repeat',
+        '-webkit-background-size': 'cover',
+        '-moz-background-size': 'cover',
+        '-o-background-size': 'cover',
+        'background-size': 'cover',
+    });
+}
+
+function addGradientToBody(){
+    $("body").css({
+        'background-image':'',
+        'border-bottom': '1px solid #ddd',
+        /* fallback */
+        'background-color': '#F2F2F2',
+        'background-repeat': 'repeat-x',
+        /* Safari 4-5, Chrome 1-9 */
+        'background': '-webkit-gradient(linear, 0% 0%, 0% 100%, from(#F2F2F2), to(#E6E6E6))',
+        /* Safari 5.1, Chrome 10+ */
+        'background': '-webkit-linear-gradient(top, #F2F2F2, #E6E6E6)',
+        /* Firefox 3.6+ */
+        'background': '-moz-linear-gradient(top, #F2F2F2, #E6E6E6)',
+        /* IE 10 */
+        'background': '-ms-linear-gradient(top, #F2F2F2, #E6E6E6)',
+        /* Opera 11.10+ */
+        'background': '-o-linear-gradient(top, #F2F2F2, #E6E6E6)'
+    });
+}
 
 function showClass(){
     class_list_page.show();
     quiz_page.hide();
     login_page.hide();
+    showTopbarButtons();
+    addGradientToBody();
 }
 
 
@@ -27,6 +73,8 @@ function showQuiz(){
     class_list_page.hide();
     quiz_page.show();
     login_page.hide();
+    showTopbarButtons();
+    addGradientToBody();
 }
 
 
@@ -51,7 +99,6 @@ $("#logout_button").click(function(){
 
 function toggleMenu(){
     menuOpen = !menuOpen;
-    console.log("1");
     if (menuOpen){
         $("#menu").animate({left:0});
     }
