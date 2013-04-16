6aeadf18da6ea0ec7d3ede97eb6a7e0d3e9ffb74
Faiz <faizamodo@gmail.com>
2013-04-15 21:52:48 -0400

    Added click listeners to the menu and add class buttons

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-    // Creates canvas 640 × 480 at 10, 50
-    var r = Raphael(10, 50, 640, 480);
-    // Creates pie chart at with center at 320, 200,
-    // radius 100 and data: [55, 20, 13, 32, 5, 1, 2]
-    r.piechart(320, 240, 100, [55, 20, 13, 32, 5, 1, 2]);
 
 
 });
 
-$("#get_data").click(function(){
-    getData();
+$("#menu_button").click(function(){
+    console.log("menu button clicked");
+})
+
+
+$("#add_class_button").click(function(){
+    console.log("add class button clicked");
 })
 
 
